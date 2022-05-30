2657fc5ddf370cf566d8015dfb8a26bad449a005
LoickDev97438 <loickcherimont@gmail.com>
2022-05-30 06:49:54 +0400

    Add date, temperature

--- scripts/meteo.js
+++ scripts/meteo.js
@@ -1,23 +1,39 @@
+// MAIN
+let body = document.querySelector("body");
 let loc = document.querySelector("#loc");
 let condition = document.querySelector("#condition"); 
 let temperature = document.querySelector("#temperature");
 let icon = document.querySelector("#icon");
+let header = document.querySelector("#header");
 
-const KEY = "<APIKEY>";
-let city = "Sainte-Clotilde,RE";
+const APIKEY = "<APIKEY>";
+let city = "Sainte-Marie,RE";
 
+// TODO: Find how to run script when user opens the browser 
+window.addEventListener('load', displayMeteo);
+
+// *** TEST ***
+// let testAPI = document.querySelector("#test");
+// testAPI.addEventListener("click", displayMeteo);
+// *** *** ***
+
+// FUNCTION
 function displayMeteo() {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${KEY}&units=metric&lang=fr`).then(response => response.json()).then(data => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}&units=metric`).then(response => response.json()).then(data => {
         console.log(data);
-        loc.innerHTML = data.name; // City name
-        condition.innerHTML = data.weather[0].main+ " " + data.weather[0].description;
-        temperature.innerHTML = `${data.main.temp} °C`;
+        header.innerHTML = `<h1>${new Date()}</h1>`;
+        loc.innerHTML = `<i class="fas fa-location-arrow"></i> ${data.name}`; // City name
+        condition.innerHTML = `${data.weather[0].main}`; // ${data.weather[0].description}
+
+        temperature.innerHTML = `<i class="fas fa-thermometer-half icon"></i> ${Math.ceil(data.main.temp)}<sup>°C</sup>`;
+
+        // <i class="fas fa-thermometer-half icon"></i>30<sup>°C</sup>
 
         let iconSrc = `http://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`;
 
         icon.innerHTML = `<img src="${iconSrc}">`;
-        icon.style.display = "inline-block";
-        icon.style.padding = "3em";
-        icon.style.backgroundColor = "#333";
+        // icon.style.display = "inline-block";
+        // icon.style.padding = "3em";
+        // icon.style.backgroundColor = "#333";
     });
 }
