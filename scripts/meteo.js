abedab736af6100fc1e2b2a582df443a8d0cc158
LoickDev97438 <loickcherimont@gmail.com>
2022-05-30 23:00:04 +0400

    User hour and erase outdated comments

--- scripts/meteo.js
+++ scripts/meteo.js
@@ -8,32 +8,38 @@ let header = document.querySelector("#header");
 
 const APIKEY = "<APIKEY>";
 let city = "Sainte-Marie,RE";
-
-// TODO: Find how to run script when user opens the browser 
+ 
 window.addEventListener('load', displayMeteo);
-
-// *** TEST ***
-// let testAPI = document.querySelector("#test");
-// testAPI.addEventListener("click", displayMeteo);
-// *** *** ***
 
 // FUNCTION
 function displayMeteo() {
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}&units=metric`).then(response => response.json()).then(data => {
         console.log(data);
-        header.innerHTML = `<h1>${new Date()}</h1>`;
+        setInterval(showTime, 1000);
         loc.innerHTML = `<i class="fas fa-location-arrow"></i> ${data.name}`; // City name
         condition.innerHTML = `${data.weather[0].main}`; // ${data.weather[0].description}
 
         temperature.innerHTML = `<i class="fas fa-thermometer-half icon"></i> ${Math.ceil(data.main.temp)}<sup>°C</sup>`;
 
-        // <i class="fas fa-thermometer-half icon"></i>30<sup>°C</sup>
-
         let iconSrc = `http://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`;
 
         icon.innerHTML = `<img src="${iconSrc}">`;
-        // icon.style.display = "inline-block";
-        // icon.style.padding = "3em";
-        // icon.style.backgroundColor = "#333";
     });
 }
+
+const showTime = function() {
+    let date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let period = "AM";
+    
+
+    if(hours > 12) {
+        hours %= 12;
+        period = "PM"; 
+    }
+
+    minutes = (minutes < 10) ? `0${minutes}`:minutes;
+
+    header.innerHTML = `<h2>${hours}:${minutes} ${period}<h2>`;
+};
