67459716ebcd3a1752ffb3da888bf46e3eb8403b
LoickDev97438 <loickcherimont@gmail.com>
2022-06-06 22:41:52 +0400

    Get API Response by reverse geocoding

--- scripts/meteo.js
+++ scripts/meteo.js
@@ -5,16 +5,46 @@ let condition = document.querySelector("#condition");
 let temperature = document.querySelector("#temperature");
 let icon = document.querySelector("#icon");
 let header = document.querySelector("#header");
-
+let searchBar = document.querySelector("#searchBar");
+let handlerBtn = document.querySelector("#handlerBtn");
+let filterForm = document.querySelector("#filterForm");
 const APIKEY = "<APIKEY>";
-let city = "Sainte-Marie,RE";
- 
+
+let lat = -20.920271083905583;
+let lon = 55.529895768669526;
+
 window.addEventListener('load', displayMeteo);
 
 // FUNCTION
 function displayMeteo() {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}&units=metric`).then(response => response.json()).then(data => {
-        console.log(data);
+
+    // if(navigator.geolocation) {
+    //     // For GEOLOCATION
+    //     var options = {
+    //         enableHighAccuracy: true,
+    //         timeout: 5000,
+    //         maximumAge: 0
+    //     };
+  
+    //     function success(pos) {
+    //         var crd = pos.coords;
+
+    //         lat = crd.latitude;
+    //         lon = crd.longitude;
+    //         console.log(lat);
+    //         console.log(lon);
+    //     }
+  
+    //     function error(err) {
+    //         console.warn(`ERROR(${err.code}): ${err.message}`);
+    //     }
+  
+    //     navigator.geolocation.getCurrentPosition(success, error, options);
+    // } else {
+    //     alert("Geolocation is not supported for this browser!");
+    // }
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIKEY}&units=metric`).then(response => response.json()).then(data => {
         setInterval(showTime, 1000);
         loc.innerHTML = `<i class="fas fa-location-arrow"></i> ${data.name}`; // City name
         condition.innerHTML = `${data.weather[0].main}`; // ${data.weather[0].description}
