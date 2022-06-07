70b99f76ebcfdd80d8825fac9daa988fcf62f9d4
LoickDev97438 <loickcherimont@gmail.com>
2022-06-07 23:08:33 +0400

    Get user location with its permission

--- scripts/meteo.js
+++ scripts/meteo.js
@@ -1,53 +1,31 @@
-// MAIN
-let body = document.querySelector("body");
+// Variables
 let loc = document.querySelector("#loc");
 let condition = document.querySelector("#condition"); 
 let temperature = document.querySelector("#temperature");
 let icon = document.querySelector("#icon");
 let header = document.querySelector("#header");
-let searchBar = document.querySelector("#searchBar");
-let handlerBtn = document.querySelector("#handlerBtn");
-let filterForm = document.querySelector("#filterForm");
+let latitude, longitude;
 const APIKEY = "<APIKEY>";
 
-let lat = -20.920271083905583;
-let lon = 55.529895768669526;
+// MAIN HANDLING: Get user location
+window.addEventListener('load', () => {
+    navigator.geolocation.getCurrentPosition(success, error);
+});
 
-window.addEventListener('load', displayMeteo);
+// USEFUL FUNCTIONS
+const success = (position) => {
+    const coordinates = position.coords;
+    latitude = coordinates.latitude;
+    console.log(latitude);
+    longitude = coordinates.longitude;
 
-// FUNCTION
-function displayMeteo() {
-
-    // if(navigator.geolocation) {
-    //     // For GEOLOCATION
-    //     var options = {
-    //         enableHighAccuracy: true,
-    //         timeout: 5000,
-    //         maximumAge: 0
-    //     };
-  
-    //     function success(pos) {
-    //         var crd = pos.coords;
-
-    //         lat = crd.latitude;
-    //         lon = crd.longitude;
-    //         console.log(lat);
-    //         console.log(lon);
-    //     }
-  
-    //     function error(err) {
-    //         console.warn(`ERROR(${err.code}): ${err.message}`);
-    //     }
-  
-    //     navigator.geolocation.getCurrentPosition(success, error, options);
-    // } else {
-    //     alert("Geolocation is not supported for this browser!");
-    // }
-
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIKEY}&units=metric`).then(response => response.json()).then(data => {
+    // Fetch conditions in user location
+    // if he/she allows it
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${APIKEY}&units=metric`).then(response => response.json()).then(data => {
+        console.log(data);
         setInterval(showTime, 1000);
         loc.innerHTML = `<i class="fas fa-location-arrow"></i> ${data.name}`; // City name
-        condition.innerHTML = `${data.weather[0].main}`; // ${data.weather[0].description}
+        condition.innerHTML = `${data.weather[0].main}`;
 
         temperature.innerHTML = `<i class="fas fa-thermometer-half icon"></i> ${Math.ceil(data.main.temp)}<sup>°C</sup>`;
 
@@ -57,7 +35,14 @@ function displayMeteo() {
     });
 }
 
-const showTime = function() {
+// User denied to get its location
+const error = () => {
+    loc.textContent = "This API won't run without your location!"; 
+    return;
+}
+
+// Show current time in format AM/PM
+const showTime = () => {
     let date = new Date();
     let hours = date.getHours();
     let minutes = date.getMinutes();
